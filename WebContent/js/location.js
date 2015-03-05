694b9dad7c9258a8f1a991e76ab1dab56838a9b6
Kai.Zhao <532388066@qq.com>
2015-03-05 16:05:47 +0800

    到底是谁

---
+++ WebContent/js/location.js
@@ -0,0 +1,30 @@
+/*-----------------------*/
+/*页面跳转*/
+/*-----------------------*/
+function world() {
+	window.location.href = 'world.html';
+}
+function pay(gold, rmb) {
+	window.location.href = 'pay.html?gold=' + gold + "&rmb=" + rmb;
+}
+function help() {
+	window.location.href = 'help.html';
+}
+function me() {
+	window.location.href = 'user.html';
+}
+function buyGold() {
+	window.location.href = 'charge.html';
+}
+function ready() {
+	window.location.href = 'ready.html';
+}
+function game() {
+	window.location.href = 'game.html';
+}
+function gameToWorld() {
+	var sure = confirm("确认退出游戏返回大厅吗?");
+	if(sure == true) {
+		window.location.href = 'world.html';
+	}
+}
---
+++ src/com/zqgame/mbm/test/Test.java
@@ -0,0 +1,5 @@
+package com.zqgame.mbm.test;
+
+public class Test {
+
+}
